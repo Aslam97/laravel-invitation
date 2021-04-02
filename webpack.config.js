709ba3de5183e39008c781aa309ec47bfa7eded5
Aslam <iupin5212@gmail.com>
2021-04-02 10:03:29 +0700

    Setup configuration development

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const CompressionPlugin = require('compression-webpack-plugin')
+
+const production = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      '@': path.join(__dirname, './resources/js'),
+      '@assets': path.join(__dirname, './resources/js/assets'),
+      '@components': path.join(__dirname, './resources/js/components'),
+      '@plugins': path.join(__dirname, './resources/js/plugins'),
+      '@router': path.join(__dirname, './resources/js/router'),
+      '@store': path.join(__dirname, './resources/js/store'),
+      '@design': path.join(__dirname, './resources/js/design'),
+      '@middleware': path.join(__dirname, './resources/js/middleware'),
+      '@utils': path.join(__dirname, './resources/js/utils'),
+      '@views': path.join(__dirname, './resources/js/views'),
+      '@layouts': path.join(__dirname, './resources/js/layouts')
+    }
+  },
+  plugins: [
+    ...(production
+      ? [
+          new CompressionPlugin({
+            filename: '[path][base].gz',
+            algorithm: 'gzip',
+            test: /\.js$|\.css$|\.html$/,
+            threshold: 10240,
+            minRatio: 0.8
+          })
+        ]
+      : [])
+  ],
+  output: {
+    chunkFilename: production
+      ? 'js/chunks/[contenthash:8].chunk.js'
+      : 'js/chunks/[name].chunk.js'
+  }
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,14 @@
-const mix = require("laravel-mix");
+const mix = require('laravel-mix')
+require('laravel-mix-vue-css-modules')
+require('laravel-mix-eslint')
 
-mix.js("resources/js/app.js", "public/js");
+mix
+  .js('resources/js/app.js', 'public/js')
+  .options({ terser: { extractComments: false } })
+  .vueCssModules({
+    oneOf: true,
+    preProcessor: { scss: true }
+  })
+  .eslint({ fix: true })
+
+mix.inProduction() ? mix.version() : mix.sourceMaps(true, 'source-map')
